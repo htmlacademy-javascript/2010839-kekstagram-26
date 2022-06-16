d5dafc251f713eb4194516a13993528094282cf7
viktor <ignatev-viktor@yandex.ru>
2022-06-16 22:21:47 +0800

    первая функция

--- js/main.js
+++ js/main.js
@@ -4,9 +4,23 @@
 
 function getRndInteger(min, max) {
   if (max <= min) {
-    'Что-то пошло не так';
+    throw new Error('Что-то пошло не так');
   } else {
-    Math.floor;
+    Math.floor();
   }
+
+  if (min < 0 || max < 0) {
+    throw new Error('Что-то пошло не так');
+  } else {
+    Math.floor();
+  }
+
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+getRndInteger();
+checkStringLength ();
